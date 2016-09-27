7c5bc5d7213a363567abdd8ecd0337a56634006d
James Stonehill <jamesstonehill@gmail.com>
2016-09-27 14:43:24 +0100

    added abbrv note method

--- model/notePad.js
+++ model/notePad.js
@@ -6,9 +6,12 @@ Notepad.prototype.addNote = function (note) {
   this.notes.push(note);
 };
 
-// Notepad.prototype.abbrNote = function (note) {
-//   if(note.length > 20) {
-//     note = note.substring(0,19)+"...";
-// }
-// return note;
-// };
+Notepad.prototype.createNote = function (noteBody) {
+  var note = new Note();
+  note.updateBody(noteBody);
+  this.addNote(note);
+};
+
+Notepad.prototype.abbrNotes = function (noteIndex) {
+  return this.notes[noteIndex].abbrNote();
+};
