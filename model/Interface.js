5550edf7f9798a41651fda16b401581f31d36cdd
Thao Vo <ttthao.vo@gmail.com>
2016-09-29 11:04:00 +0100

    Update project structure, extracted model and view from http-server repo

--- model/Interface.js
+++
@@ -1,7 +0,0 @@
-function printAbbrNotes(notepad) {
-  var abbrP = document.getElementById("abbrv")
-
-  notepad.notes.forEach( function(note) {
-    abbrP.innerHTML = abbrP.innerHTML + note.abbrNote() + "<br />" + "<br />";
-  });
-}
--- model/notePad.js
+++ model/notePad.js
@@ -1,17 +1,47 @@
 function Notepad(){
-  this.notes = [];
+
 }
 
-Notepad.prototype.addNote = function (note) {
-  this.notes.push(note);
+Notepad.prototype.note = function(){
+  document
+  .getElementById("create")
+    .addEventListener("click", function(clickEvent){
+      clickEvent.preventDefault();
+      Notepad.prototype.list();
+      Notepad.prototype.create();
+    });
+    document
+    .getElementById("test")
+    .addEventListener("click", function(clickEvent){
+      clickEvent.preventDefault();
+      document.getElementById("hider").style.visibility = "hidden";
+      // Notepad.prototype.hide();
+    });
 };
 
-Notepad.prototype.createNote = function (noteBody) {
-  var note = new Note();
-  note.updateBody(noteBody);
-  this.addNote(note);
+Notepad.prototype.create = function(){
+  var para = document.createElement("p");
+  var p = document.getElementById("para");
+  para.appendChild(document.createTextNode(document.getElementById("content").value));
+  p.appendChild(para);
 };
 
-Notepad.prototype.abbrNotes = function (noteIndex) {
-  return this.notes[noteIndex].abbrNote();
+Notepad.prototype.abbrNote = function (note) {
+  if(note.length > 20) {
+    return note.substring(0,19)+"...";
+  } else {
+    return note;
+  }
+};
+
+Notepad.prototype.list = function(){
+  var ul = document.getElementById("abbrv");
+  var li = document.createElement("li");
+  var a = document.createElement("a");
+  var abNote = document.createTextNode(Notepad.prototype.abbrNote(document.getElementById("content").value));
+  // a.setAttribute('href', "http://google.com");
+  a.appendChild(abNote);
+  li.setAttribute('id', "test");
+  li.appendChild(a);
+  ul.appendChild(li);
 };
